41b986e1ad84c1137ff5d049bb83d93952548380
Rida GHOUTI TERKI <rida.ght@gmail.com>
2020-03-18 00:04:46 +0100

    Ajout du controller Matchs

---
+++ projet_BasketBall/src/Controller/MatchsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\JoueurRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MatchsController extends AbstractController
+{
+    
+    private $joueurRepository;
+    
+    public function __construct(JoueurRepository $joueurRepository)
+    {
+        $this->joueurRepository = $joueurRepository;
+    }
+    
+    /**
+     * @Route("/joueur", name="joueur")
+     */
+    public function index()
+    {
+        $players=($this->joueurRepository->findAll());
+        return $this->render('joueur/index.html.twig', [
+            'players' => $players
+        ]);
+    }
+}
