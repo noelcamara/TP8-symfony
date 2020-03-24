7cb777fb9021e6c9c45c41659199a47cc312583d
Sedrun19 <thomas.peronny@etu.univ-lehavre.fr>
2020-03-24 18:46:02 +0100

    fonctionnalités des formulaires + authentification obligatoire

--- projet_BasketBall/src/Controller/EquipeController.php
+++ projet_BasketBall/src/Controller/EquipeController.php
@@ -2,28 +2,93 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipe;
+use App\Form\EquipeType;
 use App\Repository\EquipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/equipe")
+ */
 class EquipeController extends AbstractController
 {
-
-    private $equipeRepository;
-
-    public function __construct(EquipeRepository $equipeRepository)
+    /**
+     * @Route("/", name="equipe_index", methods={"GET"})
+     */
+    public function index(EquipeRepository $equipeRepository): Response
     {
-        $this->equipeRepository=$equipeRepository;
+        return $this->render('equipe/index.html.twig', [
+            'equipes' => $equipeRepository->findAll(),
+        ]);
     }
 
     /**
-     * @Route("/equipe", name="equipe")
+     * @Route("/new", name="equipe_new", methods={"GET","POST"})
      */
-    public function index()
+    public function new(Request $request): Response
     {
-        $equipes = ($this->equipeRepository->findAll());
-        return $this->render('equipe/index.html.twig', [
-            'equipes' => $equipes
+        $equipe = new Equipe();
+        $form = $this->createForm(EquipeType::class, $equipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($equipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('equipe_index');
+        }
+
+        return $this->render('equipe/new.html.twig', [
+            'equipe' => $equipe,
+            'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id_equipe}", name="equipe_show", methods={"GET"})
+     */
+    public function show(Equipe $equipe): Response
+    {
+        return $this->render('equipe/show.html.twig', [
+            'equipe' => $equipe,
+        ]);
+    }
+
+    /**
+     * @Route("/{id_equipe}/edit", name="equipe_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Equipe $equipe): Response
+    {
+        $form = $this->createForm(EquipeType::class, $equipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('equipe_index');
+        }
+
+        return $this->render('equipe/edit.html.twig', [
+            'equipe' => $equipe,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id_equipe}", name="equipe_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Equipe $equipe): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$equipe->getIdequipe(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($equipe);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('equipe_index');
     }
 }
--- projet_BasketBall/src/Controller/MatchsController.php
+++ projet_BasketBall/src/Controller/MatchsController.php
@@ -2,29 +2,93 @@
 
 namespace App\Controller;
 
-use App\Repository\JoueurRepository;
+use App\Entity\Matchs;
+use App\Form\MatchsType;
 use App\Repository\MatchsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/matchs")
+ */
 class MatchsController extends AbstractController
 {
-    
-    private $matchsRepository;
-    
-    public function __construct(MatchsRepository $matchsRepository)
-    {
-        $this->matchsRepository=$matchsRepository;
-    }
-    
     /**
-     * @Route("/matchs", name="matchs")
+     * @Route("/", name="matchs_index", methods={"GET"})
      */
-    public function index()
+    public function index(MatchsRepository $matchsRepository): Response
     {
-        $matchs=($this->matchsRepository->findAll());
         return $this->render('matchs/index.html.twig', [
-            'matchs' => $matchs,
+            'matchs' => $matchsRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="matchs_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $match = new Matchs();
+        $form = $this->createForm(MatchsType::class, $match);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($match);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('matchs_index');
+        }
+
+        return $this->render('matchs/new.html.twig', [
+            'match' => $match,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id_match}", name="matchs_show", methods={"GET"})
+     */
+    public function show(Matchs $match): Response
+    {
+        return $this->render('matchs/show.html.twig', [
+            'match' => $match,
+        ]);
+    }
+
+    /**
+     * @Route("/{id_match}/edit", name="matchs_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Matchs $match): Response
+    {
+        $form = $this->createForm(MatchsType::class, $match);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('matchs_index');
+        }
+
+        return $this->render('matchs/edit.html.twig', [
+            'match' => $match,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id_match}", name="matchs_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Matchs $match): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$match->getId_match(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($match);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('matchs_index');
     }
 }
