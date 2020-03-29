f2b1632f858aac4b6dd34e921847cacd9c574482
Rida GHOUTI TERKI <rida.ght@gmail.com>
2020-03-29 17:44:23 +0200

    Modification des tables et début de modification de formulaire

--- projet_BasketBall/src/Form/MatchsType.php
+++ projet_BasketBall/src/Form/MatchsType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Matchs;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +14,11 @@ class MatchsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date_match')
+            ->add('date_match'/*,TextType::class, [
+                'attr' => [
+                    'class' => 'control',
+                ]
+            ]*/)
             ->add('elim_directe')
             ->add('type')
             ->add('score_equipe1')
